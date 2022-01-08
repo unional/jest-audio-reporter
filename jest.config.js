2b6eecd44f1b8813a23f0290640033d7ebbd95cb
Homa Wong <homawong@gmail.com>
2022-01-08 12:16:55 -0800

    fix: update play-sound
    fix Support Windows environment by default #33

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,11 @@
-const isCI = require('is-ci')
 module.exports = {
+  preset: 'ts-jest',
   'collectCoverageFrom': [
     '<rootDir>/ts/**/*.[jt]s',
     '!<rootDir>/ts/bin.[jt]s',
-    '!<rootDir>/ts/type-checker/*'
   ],
   'roots': [
     '<rootDir>/ts',
-  ],
-  'reporters': isCI ? [
-    'default',
-    [
-      'jest-junit',
-      {
-        'output': '.reports/junit/js-test-results.xml',
-      },
-    ],
-  ] : [
-    'default',
-    'jest-progress-tracker',
   ],
   'testEnvironment': 'node',
   'testMatch': ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
